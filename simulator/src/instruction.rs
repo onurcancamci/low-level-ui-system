7d67336cebf1bc55e9ff29c9609b2f5b6db70189
Onurcan Camci <onurcamci55@gmail.com>
2020-09-03 00:35:27 +0300

    instruction decoding done

--- simulator/src/instruction.rs
+++ simulator/src/instruction.rs
@@ -41,8 +41,8 @@ pub(crate) enum Instruction {
     OR { rs1: u8, rs2: u8, rd: u8 }, // rs1 | rs2 -> rd
     AND { rs1: u8, rs2: u8, rd: u8 }, // rs1 & rs2 -> rd
     FENCE { rs1: u8, rd: u8, fm: u8, pred: u8, succ: u8 },
-    ECALL {},
-    EBREAK {},
+    ECALL,
+    EBREAK,
 }
 
 const mask_op: u32 = 0b1111111;
@@ -50,13 +50,17 @@ const mask_lui_imm: u32 = 0b11111111111111111111000000000000;
 const mask_rd: u32 = 0b111110000000;
 const mask_sign: u32 = 1 << 31;
 const mask_jarl_imm: u32 = 0b111111111111 << 20;
+const mask_11_0: u32 = 0b111111111111 << 20;
+const mask_11_0_extend: u32 = mask_lui_imm;
 
 impl Instruction {
     pub fn new(inst: u32) -> Self {
         let op = inst & mask_op;
         //println!("{:032b}", (u32::MAX & (0b1111111111 << 21)) >> 20);
         //println!("{:032b}", (u32::MAX & (0b0 << 21) | (1 << 20)) >> 9);
-        println!("{:032b}", 0b111111111111 << 19);
+        //println!("{:032b}", (1u32 << 31) >> (31 - 12));
+        //println!("{:032b}", (u32::MAX & (0b1111 << 8)) >> 0);
+        println!("{:032b}", 0b11111111111111111111 << 12);
         match op {
             0b0110111 => {
                 // LUI
@@ -97,27 +101,128 @@ impl Instruction {
             }
             0b1100111 => {
                 // JALR
-                unimplemented!()
+                let imm = get_imm_11_0(inst) as i32;
+                let rs1 = get_rs1(inst);
+                let rd = get_rd(inst);
+                Instruction::JALR { imm, rs1, rd }
             }
             0b1100011 => {
                 // BEQ, BNE, BLT, BGE, BLTU, BGEU
-                unimplemented!()
+                let func3 = get_func3(inst);
+                let imm = get_b_imm(inst) as i32;
+                let rs2 = get_rs2(inst);
+                let rs1 = get_rs1(inst);
+
+                match func3 {
+                    0b000 => Instruction::BEQ { imm, rs1, rs2 },
+                    0b001 => Instruction::BNE { imm, rs1, rs2 },
+                    0b100 => Instruction::BLT { imm, rs1, rs2 },
+                    0b101 => Instruction::BGE { imm, rs1, rs2 },
+                    0b110 => Instruction::BLTU { imm, rs1, rs2 },
+                    0b111 => Instruction::BGEU { imm, rs1, rs2 },
+                    _ => {
+                        panic!("Undefined B instuction");
+                    }
+                }
             }
             0b0000011 => {
                 // LB, LH, LW, LBU, LHU
-                unimplemented!()
+                let imm = get_imm_11_0(inst) as i32;
+                let rs1 = get_rs1(inst);
+                let rd = get_rd(inst);
+                let func3 = get_func3(inst);
+
+                match func3 {
+                    0b000 => Instruction::LB { imm, rs1, rd },
+                    0b001 => Instruction::LH { imm, rs1, rd },
+                    0b010 => Instruction::LW { imm, rs1, rd },
+                    0b100 => Instruction::LBU { imm, rs1, rd },
+                    0b101 => Instruction::LHU { imm, rs1, rd },
+                    _ => {
+                        panic!("Undefined L instuction");
+                    }
+                }
             }
             0b0100011 => {
                 // SB, SH, SW
-                unimplemented!()
+                let imm = get_s_imm(inst) as i32;
+                let rs1 = get_rs1(inst);
+                let rs2 = get_rs2(inst);
+                let func3 = get_func3(inst);
+
+                match func3 {
+                    0b000 => Instruction::SB { imm, rs1, rs2 },
+                    0b001 => Instruction::SH { imm, rs1, rs2 },
+                    0b010 => Instruction::SW { imm, rs1, rs2 },
+                    _ => {
+                        panic!("Undefined S instuction");
+                    }
+                }
             }
             0b0010011 => {
                 // ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
-                unimplemented!()
+                let func3 = get_func3(inst);
+                let func7 = get_func7(inst);
+                let rs2 = get_rs2(inst);
+                let imm = get_imm_11_0(inst) as i32;
+                let rs1 = get_rs1(inst);
+                let rd = get_rd(inst);
+
+                match func3 {
+                    0b000 => Instruction::ADDI { imm, rd, rs1 },
+                    0b010 => Instruction::SLTI { imm, rd, rs1 },
+                    0b011 => Instruction::SLTIU { imm, rd, rs1 },
+                    0b100 => Instruction::XORI { imm, rd, rs1 },
+                    0b110 => Instruction::ORI { imm, rd, rs1 },
+                    0b111 => Instruction::ANDI { imm, rd, rs1 },
+                    0b001 => Instruction::SLLI {
+                        shift: rs2,
+                        rs1,
+                        rd,
+                    },
+                    0b101 => {
+                        if func7 == 0 {
+                            Instruction::SRLI {
+                                shift: rs2,
+                                rs1,
+                                rd,
+                            }
+                        } else {
+                            Instruction::SRAI {
+                                shift: rs2,
+                                rs1,
+                                rd,
+                            }
+                        }
+                    }
+                    _ => {
+                        panic!("Undefined I instuction");
+                    }
+                }
             }
             0b0110011 => {
                 // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
-                unimplemented!()
+                let func7 = get_func7(inst);
+                let func3 = get_func3(inst);
+                let rs1 = get_rs1(inst);
+                let rs2 = get_rs2(inst);
+                let rd = get_rd(inst);
+
+                match func3 {
+                    0b000 if func7 == 0 => Instruction::ADD { rs2, rd, rs1 },
+                    0b000 if func7 != 0 => Instruction::SUB { rs2, rd, rs1 },
+                    0b001 => Instruction::SLL { rs2, rd, rs1 },
+                    0b010 => Instruction::SLT { rs2, rd, rs1 },
+                    0b011 => Instruction::SLTU { rs2, rd, rs1 },
+                    0b100 => Instruction::XOR { rs2, rd, rs1 },
+                    0b101 if func7 == 0 => Instruction::SRL { rs2, rd, rs1 },
+                    0b101 if func7 != 0 => Instruction::SRA { rs2, rd, rs1 },
+                    0b110 => Instruction::OR { rs2, rd, rs1 },
+                    0b111 => Instruction::AND { rs2, rd, rs1 },
+                    _ => {
+                        panic!("Undefined R instuction");
+                    }
+                }
             }
             0b0001111 => {
                 // FENCE
@@ -125,7 +230,12 @@ impl Instruction {
             }
             0b1110011 => {
                 // ECALL, EBREAK
-                unimplemented!()
+                let imm = get_imm_11_0(inst);
+                if imm == 0 {
+                    Instruction::ECALL
+                } else {
+                    Instruction::EBREAK
+                }
             }
             _ => {
                 panic!("Undefined OP Code");
@@ -137,4 +247,58 @@ impl Instruction {
 fn get_rd(inst: u32) -> u8 {
     let masked = inst & mask_rd;
     (masked >> 7) as u8
+}
+
+fn get_rs1(inst: u32) -> u8 {
+    let masked = inst & (0b11111 << 16);
+    (masked >> 16) as u8
+}
+
+fn get_rs2(inst: u32) -> u8 {
+    let masked = inst & (0b11111 << 21);
+    (masked >> 21) as u8
+}
+
+fn get_func3(inst: u32) -> u8 {
+    let masked = inst & (0b111 << 12);
+    (masked >> 12) as u8
+}
+
+fn get_func7(inst: u32) -> u8 {
+    ((inst & (0b1111111 << 25)) >> 20) as u8
+}
+
+fn get_b_imm(inst: u32) -> u32 {
+    let mut base = 0;
+    let b12 = (inst & (1u32 << 31)) >> (31 - 12);
+    let b10_5 = (inst & (0b111111 << 25)) >> 20;
+    let b11 = (inst & (1u32 << 7)) << 4;
+    let b4_1 = (inst & (0b1111 << 8)) >> 7;
+    if (inst & (1 << 31)) != 0 {
+        let ext = 0b11111111111111111111 << 12;
+        base = base | ext;
+    }
+    base = base | b12 | b4_1 | b11 | b10_5;
+    base
+}
+
+fn get_s_imm(inst: u32) -> u32 {
+    let mut base = 0;
+    let b11_5 = (inst & (0b1111111 << 25)) >> 20;
+    let b4_0 = (inst & (0b11111 << 7)) >> 7;
+    if (inst & (1 << 31)) != 0 {
+        let ext = 0b11111111111111111111 << 12;
+        base = base | ext;
+    }
+    base = base | b11_5 | b4_0;
+    base
+}
+
+fn get_imm_11_0(inst: u32) -> u32 {
+    let mut base = (inst & mask_11_0) >> 20;
+    if (inst & (1 << 31)) != 0 {
+        let ext = mask_11_0_extend;
+        base = base | ext;
+    }
+    base
 }
